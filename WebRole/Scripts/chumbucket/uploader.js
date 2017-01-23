7a3b38c65ae5ecc4e421c5c39b5ae185a59814a2
AustinBenavides <austinabenavides@gmail.com>
2017-01-22 19:12:06 -0600

    Add upload speed and bytes uploaded to table

--- WebRole/Scripts/chumbucket/uploader.js
+++ WebRole/Scripts/chumbucket/uploader.js
@@ -7,6 +7,9 @@
     this._templateClass = options['templateClass'] || 'qq-template-manual-trigger';
 
     var startTimes = {}, endTimes = {}, deltaTimes = {}, bucketNames = {};
+    // Start the counter at 19 so that it updates the UI the first time a chunk is sent
+    var uiUpdateCounter = 19;
+    var uiUpdateFreq = 20;
 
     var ref = this;
     var config = {
@@ -51,33 +54,80 @@
         callbacks: {
             onUpload: function(fileId, name) {
                 // Record the start time for submission
-                startTimes[name] = new Date();
-            },
-            onComplete: function (fileId, name, responseJSON) {
-                // TODO: see if the upload was successful
-                var success =  responseJSON['success'];
-
-                // Record the end time for submission in seconds
-                endTimes[name] = new Date();
-                deltaTimes[name] = (endTimes[name].getTime() - startTimes[name].getTime()) / 1000;
+                startTimes[fileId] = new Date();
 
                 // Create a new row in the timing table
                 var timingTable = ref.getTimingTable();
                 var newRow = timingTable.insertRow(1);
-                newRow.setAttribute('data-filename', name);
+                newRow.setAttribute("id", "file-" + fileId);
+                newRow.setAttribute("data-filename", name);
 
-                // Write the filename, status, and upload time to the new row
-                var cell1 = newRow.insertCell(0);
-                var cell2 = newRow.insertCell(1);
-                var cell3 = newRow.insertCell(2);
-                cell1.textContent = bucketNames[name] + "/" + name;
-                cell2.textContent = success ? "Complete" : "Failed";
-                cell3.textContent = deltaTimes[name] + " seconds";
+                // Create five cells in the new row (filename, status, 
+                // upload time, upload speed, bytes uploaded)
+                var filename = newRow.insertCell(0);
+                var status = newRow.insertCell(1);
+                newRow.insertCell(2);
+                newRow.insertCell(3);
+                newRow.insertCell(4);
 
-                delete startTimes[name];
-                delete endTimes[name];
-                delete deltaTimes[name];
-                delete bucketNames[name];
+                // Write the filename and status to the new row
+                if (!(fileId in bucketNames)) {
+                    bucketNames[fileId] = "default";
+                }
+                filename.textContent = bucketNames[fileId] + "/" + name;
+                status.textContent = "In Progress";
+            },
+            onProgress: function (fileId, name, uploadedBytes, totalBytes) {
+                // Only update the UI every 20 times onProgress is called
+                uiUpdateCounter++;
+                if (uiUpdateCounter >= uiUpdateFreq) {
+                    uiUpdateCounter = 0;
+                } else {
+                    return;
+                }
+
+                // Get the row for this file from the timing table
+                var curRow = document.getElementById("file-" + fileId);
+                var uploadTime = curRow.cells[2];
+                var uploadSpeed = curRow.cells[3];
+                var bytesUploaded = curRow.cells[4];
+
+                // Record the current time for the file
+                var curDate = new Date();
+                var timeElapsed = (curDate.getTime() - startTimes[fileId].getTime()) / 1000;
+                uploadTime.textContent = timeElapsed + " seconds";
+
+                // Record the average upload speed
+                uploadSpeed.textContent = ~~(uploadedBytes / timeElapsed) + " bytes per second";
+
+                // Record the number of bytes uploaded
+                bytesUploaded.textContent = uploadedBytes + "/" + totalBytes +
+                    " bytes";
+            },
+            onCancel: function() {
+                // TODO
+            },
+            onComplete: function (fileId, name, responseJson) {
+                // Determine whether the upload was successful
+                var success = responseJson['success'];
+                console.log(responseJson);
+
+                // Record the end time for submission in seconds
+                endTimes[fileId] = new Date();
+                deltaTimes[fileId] = (endTimes[fileId].getTime() - startTimes[fileId].getTime()) / 1000;
+
+                // Update the row in the table to reflect the updated status and upload time
+                var curRow = document.getElementById("file-" + fileId);
+                var status = curRow.cells[1];
+                var uploadTime = curRow.cells[2];
+                status.textContent = success ? "Complete" : "Failed";
+                uploadTime.textContent = deltaTimes[fileId] + " seconds";
+
+                // Get rid of the old info
+                delete startTimes[fileId];
+                delete endTimes[fileId];
+                delete deltaTimes[fileId];
+                delete bucketNames[fileId];
             }
         }
     };
